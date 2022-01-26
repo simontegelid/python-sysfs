5e2cdd2166179b77988e6026365efeca1c669ba2
Simon Tegelid <simon.tegelid@niradynamics.se>
2022-01-26 11:43:13 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -3,18 +3,22 @@
 from setuptools import setup
 
 setup(
-    name = 'python-sysfs',
-    version = '1',
-    author = 'The PonyCloud Team',
-    description = ('simple sysfs interface'),
-    license = 'proprietary',
-    keywords = 'system',
-    url = 'http://github.com/ponycloud/python-sysfs',
-    packages=['sysfs'],
+    name="python-sysfs",
+    version="0.0.1",
+    author="user2245775",
+    description=("simple sysfs interface"),
+    license="MIT",
+    keywords="system sysfs",
+    url="http://github.com/simontegelid/python-sysfs",
+    packages=["sysfs"],
+    project_urls={
+        "Original source": "https://stackoverflow.com/a/19330692/10901939",
+    },
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Topic :: System',
-        'License :: Other/Proprietary License',
+        "Development Status :: 4 - Beta",
+        "Topic :: System",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
     ],
 )
 
