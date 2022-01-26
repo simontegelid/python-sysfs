fb88e7d304b09cdbf17719b17ce1393592ef4bea
Simon Tegelid <simon.tegelid@niradynamics.se>
2022-01-26 11:44:13 +0100

    v0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="python-sysfs",
-    version="0.0.1",
+    version="0.0.2",
     author="user2245775",
     description=("simple sysfs interface"),
     license="MIT",
